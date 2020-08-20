160e7c127a675f6577f9aa0b98ce59e098d8b0c2
shirux <shirux123@gmail.com>
2020-08-20 17:06:41 -0500

    Updates

---
+++ precache-manifest.e52eaf6ed00a4dd36ee3666b4a6097f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2c578c0ec52847af4cfa8e4867c8ebc",
+    "url": "/react-gallery-app/index.html"
+  },
+  {
+    "revision": "632f1d62fd15fb53c425",
+    "url": "/react-gallery-app/static/css/main.6af09780.chunk.css"
+  },
+  {
+    "revision": "44985cfcb1d45a6c1b5f",
+    "url": "/react-gallery-app/static/js/2.2196ac4d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-gallery-app/static/js/2.2196ac4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "632f1d62fd15fb53c425",
+    "url": "/react-gallery-app/static/js/main.dfa2b030.chunk.js"
+  },
+  {
+    "revision": "3b3e034b11c21baff24b",
+    "url": "/react-gallery-app/static/js/runtime-main.4c864de2.js"
+  }
+]);
